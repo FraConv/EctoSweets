87969d68690e11c2429e05a8333c432e79cd7e62
FraConv <francesco16convertini@gmail.com>
2026-03-27 03:48:02 +0100

    Rewriting Header component in typescript

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,34 @@
-"use client"
-import { useState } from "react"
+"use client";
 
-export default function Header(){
+import { useState } from "react";
 
-    const [lang, setLang] = useState("it");
-    const switchLang = ()=>{
-        setLang(lang === "it" ? "gb" : "it")
-    } 
+type Lang = "it" | "gb"
 
-    return(
-        <>
-            <div className="w-full h-14 rounded-bl-[3.3rem] bg-[#242D38] flex items-center justify-between p-8">
-                <img className="h-12" src="logo/ectosweet-logo.png" alt="" />
-                <button onClick={switchLang} className="flex items-center p-1.5 border-2 border-[#00FF9C] w-9 h-9 rounded-full bg-[#00FF9C]/25">
-                    <img className="h-36" src={lang === "it" ? "flags/Flag-italy.svg" : "flags/Flag_of_the_United_Kingdom_(3-5).svg"} alt="" />
-                </button>
-            </div>
-        </>
-    )
+export default function Header() {
+  const [lang, setLang] = useState<Lang>("it");
+
+  const switchLang = (): void => {
+    setLang((prev) => (prev === "it" ? "gb" : "it"));
+  };
+
+  return (
+    <div className="w-full h-14 rounded-bl-[3.3rem] bg-[#242D38] flex items-center justify-between p-8">
+      <img className="h-12" src="logo/ectosweet-logo.png" alt="Logo" />
+
+      <button
+        onClick={switchLang}
+        className="flex items-center p-1.5 border-2 border-[#00FF9C] w-9 h-9 rounded-full bg-[#00FF9C]/25"
+      >
+        <img
+          className="h-36"
+          src={
+            lang === "it"
+              ? "flags/Flag-italy.svg"
+              : "flags/Flag_of_the_United_Kingdom_(3-5).svg"
+          }
+          alt="Language flag"
+        />
+      </button>
+    </div>
+  );
 }
