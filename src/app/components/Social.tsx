7a135afb7fd8c0d343f28ee59faa0161a1694bc7
FraConv <francesco16convertini@gmail.com>
2026-03-31 03:08:37 +0200

    Added links in social

--- src/app/components/Social.tsx
+++ src/app/components/Social.tsx
@@ -3,8 +3,19 @@ export default function Social(){
         <>
             <div className="flex justify-center mt-6">
                 <div className="flex justify-between gap-3">
-                    <img className="h-10" src="/icon/instagram.png" alt="" />
-                    <img className="h-10" src="/icon/tiktok.png" alt="" />
+                    <a 
+                        href="https://www.instagram.com/_il_ghost_creativo_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                            <img 
+                                className="h-10" 
+                                src="/icon/instagram.png" alt="" 
+                            />
+                    </a>
+                    <a href="https://www.tiktok.com/@il_ghost_creativo?is_from_webapp=1&sender_device=pc">
+                        <img 
+                            className="h-10" 
+                            src="/icon/tiktok.png" alt="" 
+                        />
+                    </a>
                 </div>
             </div>
         </>
